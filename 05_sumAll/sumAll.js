5fe5c1a7c2d782896824c698ea9dc3cdad9fa180
faceyage <alperencingoz33@gmail.com>
2022-06-01 21:02:40 +0300

    Exercise solve

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,26 @@
-const sumAll = function(min, max) {
-    //check error statements
-    if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) { 
-        return "ERROR";
+const sumAll = function (num1, num2) {
+
+
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0){
+        return "ERROR"            
     }
-    //swap numbers to make max > min
-    if (min > max) { 
-        [min, max] = [max, min];
+
+    
+    let highValue = 0;
+
+    if (num1 > num2) {
+            highValue = num1
+    } else {
+            highValue = num2
     }
-    //main algorithm
-    let sum = 0;
-    for (let i = min; i <= max; i++) {
-        sum += i;
-    }
-    return sum;
-};
+
+
+    let add = highValue*(highValue+1)
+    let total = add / 2
+
+    return total
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,25 @@
-const leapYears = function(year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0 && year % 400 != 0)
-            return false;
-        else if (year % 100 == 0 && year % 400 == 0)
-            return true;
-        else
-            return true
-    } 
+// const leapYears = function(year) {
+//     if (year % 4 == 0) {
+//         if (year % 100 == 0 && year % 400 != 0)
+//             return false;
+//         else if (year % 100 == 0 && year % 400 == 0)
+//             return true;
+//         else
+//             return true
+//     } 
 
-    return false;
+//     return false;
+// };
+
+
+const leapYears = function(years) {
+    if (years % 4 == 0 && years % 100 !== 0){
+        return true
+    } else if (years % 100 == 0 && years % 400 == 0) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
